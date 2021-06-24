87d99fe95eee8929efc1d00374b3fb62bd98e430
NotSugden <28943913+NotSugden@users.noreply.github.com>
2021-06-24 02:16:52 +0100

    fix: show destroyed bodies as dead
    refactor: unessercary type cast
    
    fix: show destroyed bodies as dead
    
    chore: remove merge conflict marker
    
    refactor: use HashSet
    
    style: remove newline
    
    refactor(MeetingIntroPatch): use init method
    
    refactor: use extension method
    
    refactor: check if game is still active

---
+++ source/Patches/MeetingIntroPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnhollowerBaseLib;
+
+using PlayerData = GameData.PlayerInfo;
+
+namespace TownOfUs
+{
+    public static class MeetingIntroPatch
+    {
+        public static List<PlayerData> DeadPlayers =
+            new List<PlayerData>();
+
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Start))]
+        public static class MeetingHud_Start
+        {
+            public static void Postfix() =>
+                Utils.ShowDeadBodies = PlayerControl.LocalPlayer.Data.IsDead;
+        }
+
+        [HarmonyPatch(typeof(MeetingIntroAnimation), nameof(MeetingIntroAnimation.Init))]
+        public static class IntroPatch
+        {
+            public static void Prefix(
+                [HarmonyArgument(1)] ref Il2CppReferenceArray<PlayerData> deadPlayers
+            )
+            {
+                var players = deadPlayers.ToHashSet();
+                players.UnionWith(DeadPlayers);
+                deadPlayers = players.ToArray();
+                DeadPlayers.Clear();
+            }
+        }
+    }
+}
