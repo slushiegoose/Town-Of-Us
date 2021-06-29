69da86fc1f12441c7916a70c1c2b8e9fb7f0436c
NotSugden <28943913+NotSugden@users.noreply.github.com>
2021-06-29 18:54:59 +0100

    fix(Underdog): change kill cooldown at the end of a meeting

--- source/Patches/ImpostorRoles/UnderdogMod/PostMeeting.cs
+++ source/Patches/ImpostorRoles/UnderdogMod/PostMeeting.cs
@@ -1,17 +1,18 @@
-using HarmonyLib;
+﻿using HarmonyLib;
 using UnityEngine;
 
 namespace TownOfUs.ImpostorRoles.UnderdogMod
 {
-    [HarmonyPatch(typeof(Object), nameof(Object.Destroy), typeof(Object))]
+    [HarmonyPatch(typeof(ExileController), nameof(ExileController.WrapUp))]
     public static class HUDClose
     {
-        public static void Postfix(Object obj)
+        public static void Postfix()
         {
-            if (ExileController.Instance == null || obj != ExileController.Instance.gameObject) return;
-            if (PlayerControl.LocalPlayer.Is(RoleEnum.Underdog))
-                PlayerControl.LocalPlayer.SetKillTimer(PlayerControl.GameOptions.KillCooldown *
-                                                       (PerformKill.LastImp() ? 0.5f : 1.5f));
+            var localPlayer = PlayerControl.LocalPlayer;
+            if (localPlayer.Is(RoleEnum.Underdog))
+                localPlayer.SetKillTimer(PlayerControl.GameOptions.KillCooldown * (
+                    PerformKill.LastImp() ? 0.5f : 1.5f
+                ));
         }
     }
 }
